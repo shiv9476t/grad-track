6a835cdc8c18a04b947cd210fe42fa25d195d5c8
Shivam Thakrar <shiv9476t@gmail.com>
2026-03-05 20:53:43 +0000

    Began developing Job class

---
+++ job.py
@@ -0,0 +1,8 @@
+class Job:
+    def __init__(self, company, title, location, salary, url):
+        self.company = company
+        self.title = title
+        self.location = location
+        self.salary = salary
+        self.url = url
+        
